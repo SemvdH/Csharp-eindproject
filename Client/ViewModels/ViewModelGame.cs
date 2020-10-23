2d4540e28e307c51f4edfed009a2380a85fd77ab
Lars <l.rook2002@hotmail.nl>
2020-10-23 21:07:12 +0200

    [DELETED] something

--- Client/ViewModels/ViewModelGame.cs
+++ Client/ViewModels/ViewModelGame.cs
@@ -197,7 +197,6 @@ namespace Client.ViewModels
             {
                 this.window.CanvasForPaint.Children.Clear();
             });
-             
         }
 
         private void ChatBox_KeyDown()
